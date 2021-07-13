bee79dcf7514313b4c93c796f829ee575fc6ced9
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2021-07-13 02:39:36 -0300

    login and logout ready

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    # path('login/', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
+    # path('logout/', views.logout, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('snippets/python/', views.language, name='language'),
     path('snippets/user/juancito/', views.user_snippets, name='user_snippets'),
     path('snippets/snippet/', views.snippet, name='snippet'),
